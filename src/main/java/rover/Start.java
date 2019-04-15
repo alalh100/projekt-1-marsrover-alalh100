9825159c2664d797849a3f25be55bb307bafeef8
alalh100 <alalh100@hhu.de>
2019-04-15 15:10:44 +0200

    take int[] p = findeRover(); outside the if und else if conditions

--- src/main/java/rover/Start.java
+++ src/main/java/rover/Start.java
@@ -13,8 +13,8 @@ public class Start {
 
 	public static void init() {
 		mars = new LinkedHashMap<>();
-		int x = 100;
-		int y = 30;
+		int x = 80;
+		int y = 20;
 		int rx = x / 2;
 		int ry = y / 2;
 		for (int i = 0; i < x; i++) {
@@ -100,8 +100,10 @@ public class Start {
 	}
 
 	public static void make(char c) {
+		int[] p = findeRover();
+
 		if (c == 'f') {
-			int[] p = findeRover();
+
 			if (get(mars, p).equals("n"))
 				p[1]--;
 			else if (get(mars, p).equals("s"))
@@ -111,7 +113,7 @@ public class Start {
 			else if (get(mars, p).equals("w"))
 				p[0]--;
 		} else if (c == 'b') {
-			int[] p = findeRover();
+
 			if (get(mars, p).equals("s"))
 				p[1]--;
 			else if (get(mars, p).equals("n"))
@@ -121,7 +123,7 @@ public class Start {
 			else if (get(mars, p).equals("e"))
 				p[0]--;
 		} else if (c == 'l') {
-			int[] p = findeRover();
+
 			if (get(mars, p).equals("n"))
 				mars.put(p, "w");
 			else if (get(mars, p).equals("s"))
@@ -131,7 +133,7 @@ public class Start {
 			else if (get(mars, p).equals("w"))
 				mars.put(p, "s");
 		} else if (c == 'r') {
-			int[] p = findeRover();
+
 			if (get(mars, p).equals("w"))
 				mars.put(p, "n");
 			else if (get(mars, p).equals("e"))
