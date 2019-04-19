a199e93b722a9155b2d0b998779c811f0f9267b7
alalh100 <alalh100@hhu.de>
2019-04-19 08:28:01 +0200

    changed the structure completely and add new function isGoodPosition the linkedhashmap mars is now a tow dimensional array and a lot of necessary changes on all of the Functions and also a rename of some Variables

--- src/main/java/rover/Start.java
+++ src/main/java/rover/Start.java
@@ -9,7 +9,9 @@ import java.util.Set;
 public class Start {
 
 	private static Random r = new Random();
-	private static LinkedHashMap<int[], String> mars;
+	private static char[][] mars;
+	private static int zeilen  = 20;
+	private static int spalten = 80;
 
 
 	public static void main(String[] args) {
@@ -18,142 +20,130 @@ public class Start {
 			long seed = Long.parseLong(args[1]);
 			r.setSeed(seed);
 		}
+
 		initField();
-		String pg = args[0];
+		String eingabe = args[0];
 		drawOutput();
-		for (int i = 0; i < pg.length(); i++) {
-			makeMovement(pg.charAt(i));
+
+		for (int i = 0; i < eingabe.length(); i++) {
+			makeMovement(eingabe.charAt(i));
 			drawOutput();
 		}
+
 	}
 
-    private static void initField() {
-		mars = new LinkedHashMap<>();
-		int x = 80;
-		int y = 20;
-		int rx = x / 2;
-		int ry = y / 2;
-		for (int i = 0; i < x; i++) {
-			for (int j = 0; j < y; j++) {
-				int[] p = new int[] { i, j };
-				if (r.nextDouble() < 0.25 && !(rx == i && ry == j)) {
-					mars.put(p, "#");
+	private static void initField() {
+
+		int mitteZeilen  = zeilen/2;
+		int mitteSpalten = spalten/2;
+        mars = new char[zeilen][spalten];
+
+
+        for (int i = 0; i < zeilen; i++) {
+			for (int j = 0; j < spalten; j++) {
+				if (r.nextDouble() < 0.25 && !(mitteZeilen == i && mitteSpalten == j)) {
+					mars[i][j]= '#';
 				}else{
-                    mars.put(p, " ");
-                }
+					mars[i][j]= ' ';
+				}
 			}
 		}
-		mars.put(new int[] { rx, ry }, "n");
-	}
-
-	private static int[] getMaximum(Set<int[]> set) {
-		int[] max = new int[2];
-		for (int[] e : set) {
-			if (e[0] > max[0])
-				max[0] = e[0];
-			if (e[1] > max[1])
-				max[1] = e[1];
-		}
-		return max;
-	}
-
-	private static String get(Map<int[], String> kloetze, int[] p) {
-		Set<Entry<int[], String>> entrySet = kloetze.entrySet();
-        String value ="";
-		for (Entry<int[], String> entry : entrySet) {
-			if (entry.getKey()[0] == p[0] && entry.getKey()[1] == p[1]) {
-                value = entry.getValue();
-            }
-		}
-        return value;
+		mars[mitteZeilen][mitteSpalten]= '^';
 	}
 
 	private static void drawOutput() {
 
-		int[] max = getMaximum(mars.keySet());
-		for (int j = 0; j < max[1]; j++) {
-			for (int i = 0; i < max[0]; i++) {
-
-				if (get(mars, new int[] { i, j }).equals(" ")){
-					System.out.print(" ");
-					continue;
-				}
-				if (get(mars, new int[] { i, j }).equals("#"))
-					System.out.print("#");
-				else if (get(mars, new int[] { i, j }).equals("n"))
-					System.out.print("^");
-				else if (get(mars, new int[] { i, j }).equals("s"))
-					System.out.print("V");
-				else if (get(mars, new int[] { i, j }).equals("e"))
-					System.out.print(">");
-				else if (get(mars, new int[] { i, j }).equals("w"))
-					System.out.print("<");
-
+		for (int j = 0; j < zeilen; j++) {
+			for (int i = 0; i < spalten; i++) {
+				System.out.print(mars[j][i]);
 			}
 			System.out.println();
 		}
-		for (int i = 0; i < max[0]; i++) {
+		for(int i = 0; i < spalten; i++) {
 			System.out.print("=");
 		}
 		System.out.println();
 	}
 
+	private static boolean isGoodPosition( int zeile , int spalte ){
 
-	private static void makeMovement(char c) {
-		int[] p = findeRover();
-		int[] tempPos = new int[] { p[0], p[1]};
-
-		if (c == 'f') {
-
-			if (get(mars, p).equals("n") && !get(mars, new int[] { tempPos[0], tempPos[1]-1} ).equals("#"))
-				p[1]--;
-			else if (get(mars, p).equals("s") && !get(mars, new int[] { tempPos[0], tempPos[1]+1} ).equals("#"))
-				p[1]++;
-			else if (get(mars, p).equals("e") && !get(mars, new int[] { tempPos[0]+1, tempPos[1]} ).equals("#"))
-				p[0]++;
-			else if (get(mars, p).equals("w") && !get(mars, new int[] { tempPos[0]-1, tempPos[1]} ).equals("#"))
-				p[0]--;
-		} else if (c == 'b') {
-
-			if (get(mars, p).equals("s") && !get(mars, new int[] { tempPos[0], tempPos[1]-1} ).equals("#"))
-				p[1]--;
-			else if (get(mars, p).equals("n") && !get(mars, new int[] { tempPos[0], tempPos[1]+1} ).equals("#"))
-				p[1]++;
-			else if (get(mars, p).equals("w") && !get(mars, new int[] { tempPos[0]+1, tempPos[1]} ).equals("#"))
-				p[0]++;
-			else if (get(mars, p).equals("e") && !get(mars, new int[] { tempPos[0]-1, tempPos[1]} ).equals("#"))
-				p[0]--;
-		} else if (c == 'l') {
-
-			if (get(mars, p).equals("n"))
-				mars.put(p, "w");
-			else if (get(mars, p).equals("s"))
-				mars.put(p, "e");
-			else if (get(mars, p).equals("e"))
-				mars.put(p, "n");
-			else if (get(mars, p).equals("w"))
-				mars.put(p, "s");
-		} else if (c == 'r') {
-
-			if (get(mars, p).equals("w"))
-				mars.put(p, "n");
-			else if (get(mars, p).equals("e"))
-				mars.put(p, "s");
-			else if (get(mars, p).equals("n"))
-				mars.put(p, "e");
-			else if (get(mars, p).equals("s"))
-				mars.put(p, "w");
-		}
-
+		if(mars[zeile][spalte]=='#')
+			return false;
+		return true;
 	}
 
+
+	private static void makeMovement( char eingabe){
+
+        int[] roverPosition = findeRover();
+        int zeile = roverPosition[0];
+        int spalte = roverPosition[1];
+
+        char direction = mars[zeile ][spalte];
+
+		if( eingabe == 'f') {
+            if(direction =='<' && isGoodPosition(zeile  , spalte-1) ) {
+                mars[zeile ][spalte-1]= direction;
+                mars[zeile ][spalte] = ' ';
+            }
+            else if(direction =='>' && isGoodPosition(zeile  , spalte+1) ) {
+                mars[zeile ][spalte+1]= direction ;
+                mars[zeile ][spalte] = ' ';
+            }
+            else if(direction =='^'  && isGoodPosition(zeile -1 , spalte)){
+                mars[zeile-1 ][spalte]= direction;
+                mars[zeile ][spalte] = ' ';
+            }
+            else if(direction =='v' && isGoodPosition(zeile +1 , spalte) ) {
+                mars[zeile+1 ][spalte]= direction;
+                mars[zeile ][spalte] = ' ';
+            }
+        }
+		else if ( eingabe == 'b'){
+
+            if(direction =='<' && isGoodPosition(zeile  , spalte+1) ) {
+                mars[zeile ][spalte+1]= direction;
+                mars[zeile ][spalte] = ' ';
+            }
+            else if(direction =='>' && isGoodPosition(zeile  , spalte-1) ) {
+                mars[zeile ][spalte+1]= direction ;
+                mars[zeile ][spalte] = ' ';
+            }
+            else if(direction =='^'  && isGoodPosition(zeile +1 , spalte)){
+                mars[zeile+1 ][spalte]= direction;
+                mars[zeile ][spalte] = ' ';
+            }
+            else if(direction =='v' && isGoodPosition(zeile -1 , spalte) ) {
+                mars[zeile-1 ][spalte]= direction;
+                mars[zeile ][spalte] = ' ';
+            }
+
+        }
+        else if(eingabe =='l'){
+            if( direction =='^') mars[zeile][spalte]='<';
+            else if( direction =='<') mars[zeile][spalte]='v';
+            else if( direction =='v') mars[zeile][spalte]='>';
+            else if( direction =='>') mars[zeile][spalte]='^';
+        }
+        else if(eingabe =='r'){
+            if(direction =='^') mars[zeile][spalte]='>';
+            else if( direction =='<') mars[zeile][spalte]='^';
+            else if( direction =='v') mars[zeile][spalte]='<';
+            else if( direction =='>') mars[zeile][spalte]='v';
+        }
+
+
+    }
+
+
 	private static int[] findeRover() {
-		Set<Entry<int[], String>> entrySet = mars.entrySet();
-		for (Entry<int[], String> entry : entrySet) {
-			if (!entry.getValue().equals(" ") && !entry.getValue().equals("#"))
-				return entry.getKey();
+
+		for (int j = 0; j < zeilen; j++) {
+			for (int i = 0; i < spalten; i++) {
+				if (mars[j][i] != ' ' && mars[j][i] != '#')
+					return new int[]{j, i};
+			}
 		}
 		throw new IllegalStateException("Rover missing in action");
 	}
-
 }
